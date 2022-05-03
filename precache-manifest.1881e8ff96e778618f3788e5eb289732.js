07f91b2d41d1964489c2bdd84c2182f68d2ded56
crocket77 <ethanpfeiffer345@outlook.com>
2022-05-02 20:54:52 -0500

    Updates

---
+++ precache-manifest.1881e8ff96e778618f3788e5eb289732.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8360056dd603121b330",
+    "url": "/crocket77/deploy_react.git/static/js/main.a8360056.chunk.js"
+  },
+  {
+    "revision": "5919557d69a5c05c9911",
+    "url": "/crocket77/deploy_react.git/static/css/1.7c240dc2.chunk.css"
+  },
+  {
+    "revision": "5919557d69a5c05c9911",
+    "url": "/crocket77/deploy_react.git/static/js/1.5919557d.chunk.js"
+  },
+  {
+    "revision": "a334157a41e033057f82",
+    "url": "/crocket77/deploy_react.git/static/js/runtime~main.a334157a.js"
+  },
+  {
+    "revision": "a6f1b22b118d5de5c45bf0bff1e5aa7e",
+    "url": "/crocket77/deploy_react.git/index.html"
+  }
+];
